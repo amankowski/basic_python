7c650c71c772308b940eb1a7b617c8482af84a2a
Niek Stortenbeker <niekns@gmail.com>
2019-03-19 12:08:20 +0100

    updated anna's files so we can merge the dumb way

--- day-2/scripts/rename_fasta_argparse.py
+++ day-2/scripts/rename_fasta_argparse.py
@@ -1,33 +1,65 @@
 #!/usr/bin/env python
 
 import argparse
+import csv
+
+def main(fasta, samples, output):
+    info = f"""
+input fasta: {fasta}
+input samples csv: {samples}
+output fasta: {output}
+"""
+    print(info)
+    names = get_new_names(samples)
+    write_new_fasta(fasta, output, names)
+    print('\nall done!')
 
 
-def run(args):
-  fastafile=open(args.fasta, 'r')
-  samples=open(args.samples, 'r')
-  fout=open(args.output, 'w')
-  
-  fasta=fastafile.readlines()
-  names=samples.readlines()
+def parse_arguments():
+    description = "Change fasta headers by sample name. "
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument("-f", "--fasta",
+                        help="fasta input file, default='fasta.fasta'",
+                        default="fasta.fasta")
+    parser.add_argument("-s", "--samples",
+                        help="comma separated file original and new header name, default='sample_names.csv'",
+                        default="sample_names.csv")
+    parser.add_argument("-o", "--output",
+                        help="new fasta file with changed headers, default='new.fasta'",
+                        default="new.fasta")
+    args = parser.parse_args()
+    return args
 
-  for line in fasta:
-    if line[0] == '>':
-      for i in names:
-        if i.split(',')[0] in line:
-          fout.write(">" + i.split(',')[1])
-    else:
-     fout.write(line)
-  fout.close()
 
-def main():
-  parser=argparse.ArgumentParser(description="change sanger seq fasta headers by sample number")
-  parser.add_argument("--fasta", help="fasta input file", dest="fasta", type=str, required=True)
-  parser.add_argument("--samples", help="tab separated file with sample number and description", dest="samples", type=str, required=True)
-  parser.add_argument("--output", help="new fasta file with changed headers", dest="output", type=str, required=True)
-  parser.set_defaults(func=run)
-  args=parser.parse_args()
-  args.func(args)
+def get_new_names(samples):
+    """make a dictionary with the old and new names of samples located in the
+    sample csv"""
+    with open(samples, 'r') as file:
+      reader = csv.reader(file)
+      names = {}
+      for row in reader:
+          old_name = row[0].strip()  # get rid of trailing spaces
+          new_name = row[1].strip()
+          names[old_name] = new_name
+    return names
+
+
+def write_new_fasta(fasta, output, names):
+    """write the lines of the fasta file to a new file, where the headers
+    that are present in the names dictionary are renamed"""
+    with open(fasta, 'r') as fin, open(output, 'w') as fout:
+      for line in fin:
+        if line[0] == '>':
+            header_name = line[1:].strip()  # remove '>' and trailing spaces
+            if header_name in names:
+                fout.write(">" + names[header_name] + '\n')
+                print(f"{header_name} was renamed to {names[header_name]}")
+            else:
+                fout.write(line)
+        else:
+          fout.write(line)
+
 
 if __name__=="__main__":
-  main()
+    args = parse_arguments()
+    main(args.fasta, args.samples, args.output)
